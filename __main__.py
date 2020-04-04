efff7b716c35fa2873fd2efd74779e23bc9c7aed
Ulbwaa <53566500+Ulbwaa@users.noreply.github.com>
2020-04-04 19:30:25 +0500

    1.0-release
    первый билд

---
+++ __main__.py
@@ -0,0 +1,197 @@
+import logging
+import hwinfo
+import requests
+import json
+
+from aiogram import Bot, Dispatcher, executor, types
+
+print('Starting...')
+
+
+def configurator():
+    hwinfo.tools.clearConsole()
+
+    Token = input('Пожалуйста, введите токен бота: ')
+    hwinfo.tools.clearConsole()
+
+    Link = input('Пожалуйста, введите ссылку на сбор: ')
+    hwinfo.tools.clearConsole()
+
+    Text = input('Пожалуйста, введите текст для кнопки: ')
+    hwinfo.tools.clearConsole()
+
+    print('Подождите, идет настройка...')
+
+    with open('config.json', 'w') as file:
+        json.dump({'botToken': Token,
+                   'donationLink': Link,
+                   'buttonText': Text}, file, indent=4)
+
+    print('Настройка завершена. Пожалуйста, перезагрузите скрипт.')
+
+
+try:
+    with open('config.json', 'r') as f:
+        config = json.loads(f.read())
+
+    botToken = config['botToken']
+    donationLink = config['donationLink']
+    buttonText = config['buttonText']
+except (KeyError, FileNotFoundError, json.decoder.JSONDecodeError):
+    configurator()
+    botToken = 'pep8 иди нахуй'
+
+logging.basicConfig(level=logging.INFO, filename='aiogram_logs')
+
+bot = Bot(token=botToken)
+
+dp = Dispatcher(bot)
+
+
+def organizer(req: str):
+    for line in req.splitlines():
+        if 'organizerName' in line:
+            return line.split(
+                'organizerName'
+            )[1].split(''
+                       '"CrowdPage">'
+                       )[1].split(
+                '</span>')[0]
+
+        else:
+            continue
+    else:
+        return None
+
+
+def description(req: str):
+    for line in req.splitlines():
+        if 'CollectMoneyPayForm__description' in line:
+            return line.split(
+                'CollectMoneyPayForm__description'
+            )[1].split(
+                '"CollectMoneyPayForm">'
+            )[1].split('</span>')[0]
+        else:
+            continue
+    else:
+        return None
+
+
+def name(req: str):
+    for line in req.splitlines():
+        if '<meta property="og:title" content="' in line:
+            return line.split(
+                '<meta property="og:title" content="'
+            )[1].split('"')[0]
+        else:
+            continue
+    else:
+        return None
+
+
+def target(req: str):
+    for line in req.splitlines():
+        if '<span class="CollectionInfoHeader__goal' in line:
+            temp_target = line.split(
+                '<span class="CollectionInfoHeader__goal'
+            )[1].split(
+                '"CollectionInfoHeader">Цель: '
+            )[1].split(' ₽</span>')[0]
+            temp = str()
+
+            for symbol in temp_target:
+                if symbol in '1234567890':
+                    temp += symbol
+
+            return int(temp)
+        else:
+            continue
+    else:
+        return None
+
+
+def collected(req: str):
+    for line in req.splitlines():
+        if 'class="CollectionInfoHeader__percents' in line:
+            temp_collected = line.split(
+                '"CollectionInfoHeader">Собрано: '
+            )[1].split(' ₽</span>')[0]
+
+            temp = str()
+
+            for symbol in temp_collected:
+                if symbol in '1234567890':
+                    temp += symbol
+
+            return int(temp)
+        else:
+            continue
+    else:
+        return None
+
+
+def progressPercentage(collect, targ):
+    try:
+        return round(collect / targ * 100)
+    except TypeError:
+        return None
+
+
+def progressBar(progressPercent):
+    try:
+        if progressPercent >= 100:
+            return '████████████████████'
+        else:
+            collect = (round(progressPercent / 5)) * '█'
+            residual = (20 - len(collect)) * '░'
+            return collect + residual
+    except TypeError:
+        return None
+
+
+@dp.message_handler(content_types=['text'])
+async def text_handler(message: types.Message):
+    inline_kb_full = types.InlineKeyboardMarkup(row_width=1)
+    inline_kb_full.add(
+        types.InlineKeyboardButton(buttonText, url=donationLink))
+
+    tempMessage = await message.reply('<i>Загрузка...</i>', parse_mode='html')
+
+    request = requests.get(donationLink).text
+
+    donationName = name(request)
+    donationOrganizer = organizer(request)
+    donationDescription = description(request)
+    donationTarget = target(request)
+    donationCollected = collected(request)
+    donationProgressPercentage = progressPercentage(donationCollected, donationTarget)
+    donationProgressBar = progressBar(donationProgressPercentage)
+
+    message = str()
+
+    if donationName is not None:
+        message += f'<b>{donationName}</b>\n\n'
+
+    if donationOrganizer is not None:
+        message += f'<b>Организатор сбора:</b> <i>{donationOrganizer}</i>\n'
+
+    if donationTarget is not None:
+        message += f'<b>Цель:</b> <i>{donationTarget}₽</i>\n'
+
+    if donationCollected is not None:
+        message += f'<b>Собрано:</b> <i>{donationCollected}₽</i>\n\n'
+
+    if donationProgressBar is not None and donationProgressPercentage is not None:
+        message += f'<b>Прогресс:</b>\n<b>{donationProgressBar}</b> {donationProgressPercentage}%\n\n'
+
+    if donationDescription is not None:
+        message += f'<b>Описание:</b>\n<i>{donationDescription}</i>'
+
+    return await tempMessage.edit_text(message, parse_mode='html', reply_markup=inline_kb_full)
+
+
+if __name__ == '__main__':
+    hwinfo.tools.clearConsole()
+    print('Started!')
+    executor.start_polling(dp, skip_updates=True)
---
+++ hwinfo.py
@@ -0,0 +1,305 @@
+import os
+import platform
+import socket
+import subprocess
+import time
+import random
+
+import http.client
+import psutil
+
+import speedtest as speed
+
+version = '1.4-release'
+git = 'https://github.com/Ulbwaa/HW-Info'
+projects = 'https://ulbwa.suicide.today/projects/'
+
+
+class tools:
+    @staticmethod
+    def clearConsole():
+        if psutil.WINDOWS:
+            return os.system("cls")
+        else:
+            return os.system("clear")
+
+    @staticmethod
+    def checkOutput(command: str):
+        try:
+            return subprocess.check_output(command, shell=True,
+                                           universal_newlines=True,
+                                           stderr=subprocess.DEVNULL)
+
+        except subprocess.CalledProcessError:
+            return False
+
+
+def speedtest(htmlMarkup=True):
+    tester = speed.Speedtest()
+    tester.get_best_server()
+    tester.download(threads=None)
+    tester.upload(threads=None)
+
+    download = round(tester.results.dict()["download"] / 2 ** 20)
+    upload = round(tester.results.dict()["upload"] / 2 ** 20)
+    ping = round(tester.results.dict()["ping"])
+    server = tester.results.dict()["server"]["country"] + ', ' + tester.results.dict()["server"]["name"]
+
+    output = 'HW-Info SpeedTester\n' \
+             '-------------------\n'
+
+    if htmlMarkup:
+        output += f'<b>Download</b>: <code>{download} MiB/s</code>\n' \
+                  f'<b>Upload</b>: <code>{upload} MiB/s</code>\n' \
+                  f'<b>Ping</b>: <code>{ping}ms</code>\n' \
+                  f'<b>Server</b>: <code>{server}</code>'
+    else:
+        output += f'Download: {download} MiB/s\n' \
+                  f'Upload: {upload} MiB/s\n' \
+                  f'Ping: {ping}ms\n' \
+                  f'Server: {server}'
+
+    return output
+
+
+def hwinfo(htmlMarkup=True, showThreadsPercentage=True):
+    if psutil.WINDOWS:
+        command = 'powershell neofetch --stdout'
+    else:
+        command = 'neofetch --stdout'
+
+    output = tools.checkOutput(command)
+
+    if output:
+        fetch = output.split('- \n')[0] + '- '
+        out = output.split('- \n')[0] + '- '
+
+        for i in output.split('- \n')[1].split('\n'):
+            if i != '' and i != ' ':
+                j = i.split(': ')[0]
+                y = i.split(': ')[1]
+
+                if j == 'CPU':
+                    fetch += f'\n{j}: {y}'
+                    fetch += f'\nCPU Load: {_CPULoad()}'
+                    fetch += f'\nCPU Architecture: {_CPUArch()}'
+
+                elif j == 'Memory':
+                    fetch += f'\n{j}: {y}'
+                    fetch += f'\nSWAP: {_swap()}'
+                    fetch += f'\nStorage: {_storage()}'
+
+                elif j == 'Kernel':
+                    fetch += f'\n{j}: {y}'
+
+                    if _users():
+                        fetch += f'\nUsers: {_users()}'
+
+                    if not _IPs_Check():
+                        fetch += f'\nGlobal IP: {_http_ip()}'
+                        fetch += f'\nLocal IP: {_LocalIP()}'
+                    else:
+                        fetch += f'\nLocal / Global IP: {_http_ip()}'
+
+                    if _mother_board():
+                        fetch += f'\nMotherboard: {_mother_board()}'
+
+                elif j == 'Uptime':
+                    fetch += f'\n{j}: {y}'
+                    fetch += f'\nBoot Time: {_BootTime()}'
+
+                elif j == 'Host' and 'Hackintosh' in y:
+                    pass
+
+                else:
+                    fetch += f'\n{j}: {y}'
+
+        fetch += f'\nPython version: {_python_version()}'
+
+        if _where_python():
+            fetch += f'\nPython path: {_where_python()}'
+
+        if _java_version():
+            fetch += f'\nJAVA version: {_java_version()}'
+
+        if _where_java():
+            fetch += f'\nJAVA path: {_where_java()}'
+
+        fetch += f'\nHW-Info version: {_hwinfo_version()}'
+
+        if showThreadsPercentage:
+            fetch += '\n'
+            for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
+                fetch += f'\nThread {i + 1} load: {round(percentage)}%'
+
+        for i in fetch.split('- \n')[1].split('\n'):
+            if i != '' and i != ' ':
+                j = i.split(': ', maxsplit=1)[0]
+                y = i.split(': ', maxsplit=1)[1]
+                if htmlMarkup:
+                    out += f'\n<b>{j}</b>: <code>{y}</code>'
+
+                else:
+                    out += f'\n{j}: {y}'
+            else:
+                out += '\n'
+
+        if htmlMarkup and random.randint(1, 15) == 5:
+            out += '\n\n<b>HW-Info</b> is an <b>open source project</b>. ' \
+                   'You can use this module in your projects by downloading it on <b>GitHub!</b> ' \
+                   f'Link to download source: <code>{git}</code>. ' \
+                   f'My other projects are available for review at the following link: ' \
+                   f'<code>{projects}</code>.'
+
+        return out
+    else:
+        if htmlMarkup:
+            return '<b>Neofetch is not installed!</b>'
+        else:
+            return 'Neofetch is not installed!'
+
+
+def _IPs_Check():
+    return str(_LocalIP()) == str(_http_ip())
+
+
+def _LocalIP():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except ConnectionError:
+        ip = '127.0.0.1'
+
+    return str(ip)
+
+
+def _users():
+    try:
+        users = []
+
+        for user in psutil.users():
+            if str(user.name) not in users:
+                users.append(str(user.name))
+
+        if users == [] or len(users) <= 0:
+            return False
+    except (Exception, BaseException):
+        return False
+
+    return ', '.join(users)
+
+
+def _CPUArch():
+    return platform.uname().machine.replace('_', '-')
+
+
+def _CPULoad():
+    return f'{round(psutil.cpu_percent(0.5))}%'
+
+
+def _BootTime():
+    return time.ctime(psutil.boot_time())
+
+
+def _swap():
+    all_ = round(psutil.swap_memory().total / 1e+6)
+    used = round(psutil.swap_memory().used / 1e+6)
+
+    return f'{used}MiB / {all_}MiB '
+
+
+def _storage():
+    all_ = round(psutil.disk_usage('/.').total / 1e+9)
+    used = round(psutil.disk_usage('/.').used / 1e+9)
+
+    return f'{used}GiB / {all_}GiB '
+
+
+def _python_version():
+    return '{} ver. {}'.format(platform.python_implementation(),
+                               platform.python_version()).split('ver. ')[1]
+
+
+def _java_version():
+    command = 'java -version 2>&1 | awk -F[\\\"_] \'NR==1{print $2}\''
+    JAVA_temp = tools.checkOutput(command)
+
+    if JAVA_temp:
+        return JAVA_temp.split('\n')[0]
+    else:
+        return False
+
+
+def _http_ip():
+    conn = http.client.HTTPConnection("ifconfig.me")
+    conn.request("GET", "/ip")
+    return conn.getresponse().read().decode('UTF8')
+
+
+def _hwinfo_version():
+    return version
+
+
+def _mother_board():
+    if psutil.WINDOWS:
+        command = 'wmic baseboard get Manufacturer'
+        mother = tools.checkOutput(command)
+
+        if mother:
+            manuf = mother.split('\n')[2]
+        else:
+            return False
+
+        command = 'wmic baseboard get product'
+        mother = tools.checkOutput(command)
+
+        if mother:
+            module = mother.split('\n')[2]
+        else:
+            return False
+
+        return manuf + ' ' + module
+    else:
+        return False
+
+
+def _where_python():
+    command = 'where python3'
+    output = tools.checkOutput(command)
+
+    if output:
+        return output.split('\n')[0]
+    else:
+        command = 'whereis python3'
+        output = tools.checkOutput(command)
+
+        if output:
+            return output.split('\n')[0].split(' ')[1]
+        else:
+            return False
+
+
+def _where_java():
+    command = 'where java'
+    output = tools.checkOutput(command)
+
+    if output:
+        return output.split('\n')[0]
+    else:
+        command = 'whereis java'
+        output = tools.checkOutput(command)
+
+        if output:
+            return output.split('\n')[0].split(' ')[1]
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    tools.clearConsole()
+    print('loading...')
+    hw = hwinfo(False, False)
+    tools.clearConsole()
+    print(hw)
+    exit(0)
